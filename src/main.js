2f4588657b22cd68c6a8486d933864a29064c26d
Samuel Wafula <samuelwafula@Samuels-MacBook-Air.local>
2025-05-02 11:56:52 +0300

    add dynamic meta and og

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
+import './assets/css/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/css/main.css'
 import { createPinia } from 'pinia'
+import { createHead } from '@vueuse/head'
 
 const app = createApp(App)
+const head = createHead()
+
 app.use(router)
 app.use(createPinia())
+app.use(head)
 app.mount('#app')
--- src/views/EventDetailsView.vue
+++ src/views/EventDetailsView.vue
@@ -7,6 +7,7 @@
   import { useRoute } from 'vue-router';
   import { ref, onMounted } from 'vue';
   import axios from 'axios'
+  import { useHead } from '@vueuse/head';
 
   export default {
     setup() {
@@ -18,6 +19,21 @@
         try {
           const { data } = await axios.get('https://run.mocky.io/v3/474a379b-8894-47c9-a99c-39939a947bfd');
           event.value = data.find(e => e.id === id)
+
+          if(event.value){
+            useHead({
+              title: event.value.event,
+                meta: [
+                  { name: 'description', content: event.value.description },
+                  { property: 'og:title', content: event.value.event },
+                  { property: 'og:description', content: event.value.description },
+                  { property: 'og:image', content: event.value.image },
+                  { property: 'og:type', content: 'website' },
+                  { property: 'og:url', content: window.location.href }
+                ]
+            })
+          }
+
         } catch(err){
           console.error(err)
         }
