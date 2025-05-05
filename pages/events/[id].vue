065ce66f09aadfeae6f919bec1133809adbe7fe5
Samuel Wafula <samuelwafula@Samuels-MacBook-Air.local>
2025-05-05 21:47:54 +0300

    static to dynamic

--- pages/events/[id].vue
+++ pages/events/[id].vue
@@ -23,31 +23,34 @@
           return events.find((e) => e.id === parseInt(id)) || null
       }, { initialCache: true })
 
-      useHead({
-        title: `${event.event}`,
-        meta: [
-          { name: 'title', content: `${event.event}` },
-          { name: 'description', content: `${event.description}`},
+      watchEffect(()=> {
+        if(!event.value) return
 
-          // Open Graph / Facebook
-          { property: 'og:type', content: 'website' },
-          { property: 'og:url', content:  `${baseUrl}${route.fullPath}` },
-          { property: 'og:title', content: `${event.event}` },
-          { property: 'og:description', content:`${event.description}` },
-          { property: 'og:image', content: `${event.image}` },
+        useHead({
+          title: `${event.event}`,
+          meta: [
+            { name: 'title', content: `${event.value.event}` },
+            { name: 'description', content: `${event.value.description}`},
 
-          // Twitter
-          { property: 'twitter:card', content: 'summary_large_image' },
-          { property: 'twitter:url', content:  `${baseUrl}${route.fullPath}` },
-          { property: 'twitter:title', content: `${event.event}` },
-          { property: 'twitter:description', content: `${event.description}`},
-          { property: 'twitter:image', content: `${event.image}` }
-        ],
-        link: [
-          { rel: 'canonical', href:  `${baseUrl}${route.fullPath}` }
-        ]
+            // Open Graph / Facebook
+            { property: 'og:type', content: 'website' },
+            { property: 'og:url', content:  `${baseUrl}${route.fullPath}` },
+            { property: 'og:title', content: `${event.value.event}` },
+            { property: 'og:description', content:`${event.value.description}` },
+            { property: 'og:image', content: `${event.value.image}` },
+
+            // Twitter
+            { property: 'twitter:card', content: 'summary_large_image' },
+            { property: 'twitter:url', content:  `${baseUrl}${route.fullPath}` },
+            { property: 'twitter:title', content: `${event.value.event}` },
+            { property: 'twitter:description', content: `${event.value.description}`},
+            { property: 'twitter:image', content: `${event.value.image}` }
+          ],
+          link: [
+            { rel: 'canonical', href:  `${baseUrl}${route.fullPath}` }
+          ]
+        })
       })
-
 
       return {
         event,
