6f9642dacc8e1ed9f942aad482ed50f5145a4a9f
Samuel Wafula <samuelwafula@Samuels-MacBook-Air.local>
2025-05-06 09:45:03 +0300

    updated

--- pages/events/[id].vue
+++ pages/events/[id].vue
@@ -27,32 +27,18 @@
         title: 'Sample Event | Event Details',
         meta: [
           { name: 'title', content: 'Sample Event | Event Details' },
-          {
-            name: 'description',
-            content:
-              "Join us for an electrifying night at the Amapiano Groove (Pretty Gals Big Boyz Edition), where the beats are fresh, the vibes are unmatched, and the energy is contagious. It's where BLENDS, CULTURE AND GROOVE"
-          },
+          { name: 'description', content: " test description" },
 
-          // Open Graph / Facebook
           { property: 'og:type', content: 'website' },
           { property: 'og:url', content: 'https://shop-oceanscan.onrender.com/events/1' },
           { property: 'og:title', content: 'Sample Event | Event Details' },
-          {
-            property: 'og:description',
-            content:
-              "Join us for an electrifying night at the Amapiano Groove (Pretty Gals Big Boyz Edition), where the beats are fresh, the vibes are unmatched, and the energy is contagious. It's where BLENDS, CULTURE AND GROOVE"
-          },
+          { property: 'og:description', content: "test description" },
           { property: 'og:image', content: 'https://madfun.imgix.net/Roast_House_Comedy_984.jpeg?w=412&h=412&fit=crop&auto=format' },
 
-          // Twitter
           { property: 'twitter:card', content: 'summary_large_image' },
           { property: 'twitter:url', content: 'https://shop-oceanscan.onrender.com/events/1' },
           { property: 'twitter:title', content: 'Sample Event | Event Details' },
-          {
-            property: 'twitter:description',
-            content:
-              "Join us for an electrifying night at the Amapiano Groove (Pretty Gals Big Boyz Edition), where the beats are fresh, the vibes are unmatched, and the energy is contagious. It's where BLENDS, CULTURE AND GROOVE"
-          },
+          { property: 'twitter:description', content: "test description" },
           { property: 'twitter:image', content: 'https://madfun.imgix.net/Roast_House_Comedy_984.jpeg?w=412&h=412&fit=crop&auto=format' }
         ],
         link: [
